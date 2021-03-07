59a30cf683f0f7a520971dc9d42460b36bd7d1f6
WhiteThunder <whetethunger@gmail.com>
2021-03-07 15:44:44 -0800

    Update plugin description

--- DroneStorage.cs
+++ DroneStorage.cs
@@ -12,7 +12,7 @@ using UnityEngine;
 namespace Oxide.Plugins
 {
     [Info("Drone Storage", "WhiteThunder", "1.0.0")]
-    [Description("Allows players to manually or automatically deploy a small stash to deployable drones.")]
+    [Description("Allows players to deploy a small stash to RC drones.")]
     internal class DroneStorage : CovalencePlugin
     {
         #region Fields
