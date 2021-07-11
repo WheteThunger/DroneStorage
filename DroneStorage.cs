e3c4ce37864fe1afad3739b30b9816851d6b9d00
WhiteThunder <whetethunger@gmail.com>
2021-07-11 00:44:56 -0700

    Increment minor version

--- DroneStorage.cs
+++ DroneStorage.cs
@@ -12,7 +12,7 @@ using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("Drone Storage", "WhiteThunder", "1.0.2")]
+    [Info("Drone Storage", "WhiteThunder", "1.1.0")]
     [Description("Allows players to deploy a small stash to RC drones.")]
     internal class DroneStorage : CovalencePlugin
     {
